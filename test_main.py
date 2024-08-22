bdfb760484ef3e322d1824b6ebb8797f4bd2833d
Niharika <43085656+nih326@users.noreply.github.com>
2024-08-22 21:43:04 +0000

    sample1

--- test_main.py
+++ test_main.py
@@ -7,4 +7,4 @@ def test_is_prime_pass():
     assert is_prime(5) == True  # This should pass
 
 def test_is_prime_fail():
-    assert is_prime(8) == True  # This should fail, as 4 is not a prime number
+    assert is_prime(7) == True  # This should fail, as 4 is not a prime number
